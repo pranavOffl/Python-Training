cd1a9af2db7704f857fd4053cf50d3bb628e68f2
pranavOffl <pranav.g@toadsters.com>
2026-01-15 22:01:36 +0530

    lambda  py file

--- Jan_15_2026/lambda.py
+++ Jan_15_2026/lambda.py
@@ -1,2 +1,44 @@
 x = lambda a, b, c : a + b + c
 print(x(5, 5, 10))
+
+# Map - Transform each element (Many -> Many)
+
+nums = [1, 2, 3, 4]
+
+def square(x):
+    return x * x
+
+result = list(map(square, nums))
+print(result)
+
+result = list(map(lambda x : x * x, nums))
+print(result)
+
+# filter() – Select elements (Many -> Fewer)
+
+nums = [1, 2, 3, 4, 5]
+
+def is_even(x):
+    return x % 2 == 0
+
+result = list(filter(is_even, nums))
+print(result)
+
+result = list(filter(lambda x : x % 2 == 0, nums))
+print(result)
+
+# reduce() – Combine into one value (Many -> One)
+
+from functools import reduce
+
+nums = [1, 2, 3, 4]
+
+def add(a, b):
+    return a + b
+
+result = reduce(add, nums)
+print(result)
+
+result = reduce(lambda a, b : a + b, nums)
+print(result)
+
