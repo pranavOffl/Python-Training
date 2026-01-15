3690cd92775299d062835553540d4f3e54747bd4
pranavOffl <pranav.g@toadsters.com>
2026-01-15 16:25:09 +0530

    datatypes py file

---
+++ Jan_15_2026/datatypes.py
@@ -0,0 +1,15 @@
+# int
+num_int = 10
+print(num_int, type(num_int))
+
+# float
+num_float = 4.5
+print(num_float, type(num_float))
+
+# string
+num_string = "pranav"
+print(num_string, type(num_string))
+
+# boolean
+num_bool = True # or false
+print(num_bool, type(num_bool))
