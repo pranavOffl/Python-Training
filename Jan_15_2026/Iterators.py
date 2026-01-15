c9cedb6c48359a7c00e0a40882fdf358b55251db
pranavOffl <pranav.g@toadsters.com>
2026-01-15 22:11:58 +0530

    Iterators py file

---
+++ Jan_15_2026/Iterators.py
@@ -0,0 +1,14 @@
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x) 
