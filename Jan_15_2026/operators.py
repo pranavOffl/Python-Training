efab01331513827f78cf494909f58303fb4e1017
pranavOffl <pranav.g@toadsters.com>
2026-01-15 16:39:40 +0530

    operators py file

---
+++ Jan_15_2026/operators.py
@@ -0,0 +1,41 @@
+# Arithmetic Operators
+
+a, b = 5, 10
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a / b = {a / b}')
+print(f'a // b = {a // b}')
+print(f'a * b = {a * b}')
+print(f'a % b = {a % b}')
+print(f'a ** b = {a ** b}')
+
+# Assignment Operators
+
+x = 5
+x += 3
+x -= 2
+x *= 1
+x /= 4
+x %= 7
+x //= 8
+x **= 2
+
+# Comparison Operators
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+# Logical Operators
+
+x = 6
+
+print(x > 0 and x < 10)
+print(x > 0 or x < 10)
+print(not(x < 0))
